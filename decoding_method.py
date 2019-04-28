62ea9d0153b7b71e6e00ff8c0ef1b887b661a7ac
li-ming-fan <li_m_f@163.com>
2019-04-28 22:57:22 +0800

    modified:   decoding_method.py

--- decoding_method.py
+++ decoding_method.py
@@ -15,17 +15,20 @@ def do_greedy_decoding(model, src, src_mask, max_len,
     memory = model.encode(src, src_mask)
     
     batch_start = tf.cast(tf.ones_like(src[:, 0:1]), dtype = tf.int64)
-    batch_start = tf.multiply(batch_start, start_symbol_id)
+    batch_start = tf.multiply(batch_start, start_symbol_id)    
+    pad_tokens = tf.cast(tf.zeros_like(src[:, 0:1]), dtype = tf.int64)
+    pad_tokens = tf.tile(pad_tokens, [1, max_len-1])
     
     logits_list = []
     preds_list = []
     
-    dcd_feed = batch_start
-    for step in range(max_len):
-        mask_subsequent = subs_masks[step]
-        crs_mask = dcd_crs_masks[step]
+    mask_subsequent = subs_masks[-1]
+    crs_mask = dcd_crs_masks[-1]
+    
+    dcd_feed = tf.concat([batch_start, pad_tokens], 1)
+    for step in range(max_len):        
         out = model.decode(dcd_feed, mask_subsequent, memory, crs_mask)
-        out_last = out[:, -1, :]
+        out_last = out[:, step, :]
         logits_curr = model.generator.forward(out_last)
         logits_curr = tf.nn.softmax(logits_curr, -1)
         preds_curr = tf.argmax(logits_curr, -1)
@@ -34,10 +37,10 @@ def do_greedy_decoding(model, src, src_mask, max_len,
         preds_list.append(preds_curr)
         #
         preds = tf.transpose(tf.stack(preds_list, 0), [1, 0])
-        dcd_feed = tf.concat([batch_start, preds], 1)
+        dcd_feed = tf.concat([batch_start, preds, pad_tokens[:, step+1:]], 1)
         #
     #
-    logits = tf.transpose(tf.stack(logits_list, 0), [1, 0, 2])    
+    logits = tf.transpose(tf.stack(logits_list, 0), [1, 0, 2])
     return logits, preds
 
 
